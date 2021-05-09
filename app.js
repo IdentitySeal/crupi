947758d9f5caacc71c1dfb86d59cc11d2b1cc1df
unknown <teemealeheen@gmail.com>
2021-05-09 18:40:21 +0100

    Added a Procfile.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 });
 require('./routes')(app);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT ||PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
